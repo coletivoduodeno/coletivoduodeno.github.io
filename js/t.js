5f5e535b44d3b10a70cbf5e284a17b2e5943ab7c
coletivoduodeno <36006643+coletivoduodeno@users.noreply.github.com>
2018-02-01 18:08:12 -0200

    Update t.js

--- js/t.js
+++ js/t.js
@@ -1,40 +1,5 @@
-$(function() {
-	var $container = $('#container');
-// init
-$container.isotope({
-  // options
-  itemSelector: '#container > div',
-  layoutMode: 'fitRows'
+$('#demo_1').t({
+blink:300,
+caret:'<span style="color:blue;">\u2589</span>',
+pause_on_click:true
 });
-	$(".filter").click(function(){
-		//$(".tt-grid li").css({"display": "none"});
-	/*	$(".tt-grid li").addClass("hide");
-		$(".filter").removeClass("highlight");
-		//$("."+$(this).text()).css({"display": "inline-block"});
-		$("."+$(this).text()).removeClass("hide");
-		$(this).addClass("highlight");
-		$("#show-all").removeClass("highlight");*/
-		//alert("clicked "+ $(this).text());
-		$(".filter").removeClass("highlight");
-		$("#show-all").removeClass("highlight");
-		$(this).addClass("highlight");
-		$container.isotope({ filter: "."+$(this).text() });
-		//alert($(this).text());
-	});
-
-	$("#show-all").click(function(){
-		//$(".tt-grid li").css({"display": "inline-block"});
-		$(".filter").removeClass("highlight");
-		$(this).addClass("highlight");
-		//$(".tt-grid li").removeClass("hide");
-		$container.isotope({ filter: '#container > div' });
-	});
-
-});
-
-
-function initGrid(){
-	$(".tt-grid li" ).each(function( i ) {
-		
-	});
-}
